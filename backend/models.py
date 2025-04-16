c2e104ec81a442892420489e7b4e2ee03670c769
DivyaPatel28 <157919710+DivyaPatel28@users.noreply.github.com>
2025-04-16 16:11:08 -0400

    Delete backend directory

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-from datetime import datetime
-
-class Transaction(BaseModel):
-    amount: float
-    category: str
-    type: str  # "income" or "expense"
-    date: Optional[datetime] = datetime.now()
